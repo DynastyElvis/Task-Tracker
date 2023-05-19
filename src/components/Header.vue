f4b36d276770901cce0ad6e7c14852febb7b255c
kipkemoilvs <kipkemoilvs@gmail.com>
2023-05-19 12:34:53 +0300

    ft:button and header log

---
+++ src/components/Header.vue
@@ -0,0 +1,31 @@
+<template>
+    <header>
+        <h1>{{ title }}</h1>
+        </Button>
+    </header>
+  </template>
+  
+  <script>
+  import Button from './Button'
+
+  export default {
+    name: 'AppHeader',
+    props: {
+    title: String,
+    showAddTask: Boolean,
+  },
+  components: {
+    Button,
+  },
+  }
+  </script>
+  
+  <style scoped>
+  header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 20px;
+  }
+  </style>
+  
